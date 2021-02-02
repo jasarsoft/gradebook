6ab5aba7d181a5a26632f2124877b86008355790
Edin Jašarević <edinjasar14@gmail.com>
2021-02-02 23:49:38 +0100

    passing parameters by references

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -5,13 +5,27 @@ namespace GradeBook.Tests
 {
     public class TypeTests
     {
-         [Fact]
-        public void Test1()
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(out book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void GetBookSetName(out Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CSharpIsPassByValue()
         {
             var book1 = GetBook("Book 1");
             GetBookSetName(book1, "New Name");
 
-            Assert.Equal("New Nam", book1.Name);
+            Assert.Equal("Book 1", book1.Name);
         }
 
         private void GetBookSetName(Book book, string name)
